362d91d6c9c982decccda3321d38bd811fdb89fa
potzplitz <127513690+potzplitz@users.noreply.github.com>
2024-03-02 23:13:09 +0100

    loadwindow

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,21 +6,32 @@ import data.DownloadLevels;
 import data.FetchData;
 import data.ManageFiles;
 import filestructure.CreateFileStructure;
+import gui.LoadMenu;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
+		LoadMenu load = new LoadMenu();
+		load.onLoad();
+		
+		load.updateBar("Dateistruktur wird gebaut...");
+		
 		CreateFileStructure struct = new CreateFileStructure();
 		struct.create();
+		
+		load.updateBar("Updatedaten werden heruntergeladen...");
 		
 		FetchData fetch = new FetchData();
 		fetch.getGithubString();
 		
+		load.updateBar("Liste wird auf Updates geprüft...");
+		
 		ManageFiles manager = new ManageFiles();
 		manager.compareArrays();
 
-		
+		load.updateBar("Ladevorgang abgeschlossen");
+		load.close();
 		
 		
 	}
