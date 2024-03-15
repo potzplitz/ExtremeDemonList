00f42c6bcda129204b7aa51bca085a0099c68d32
potzplitz <127513690+potzplitz@users.noreply.github.com>
2024-03-15 22:44:19 +0100

    df

--- src/gui/SettingsGui.java
+++ src/gui/SettingsGui.java
@@ -1,5 +1,22 @@
 package gui;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import settingsfunctions.DeleteDatabase;
+
 public class SettingsGui {
+	
+	public void showSettings() {
+		JFrame settings = new JFrame("Einstellungen");
+		settings.setLayout(null);
+		settings.setResizable(false);
+		settings.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		settings.setSize(500, 500);
+		settings.setVisible(true);
+	}
 
 }
